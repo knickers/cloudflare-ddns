4ba5d0df469c93d1b84038312a4a8961d91cd101
Nick Cox <nickcox1008@gmail.com>
2015-02-24 16:37:48 -0700

    remove all the testing stuff; add the record id to the example config

--- update.sh
+++ update.sh
@@ -7,31 +7,18 @@ do
 	CF["${line%=*}"]="${line#*=}"
 done < cloudflare.conf
 
-set -x
 URL=${CF[subdomain]:+${CF[subdomain]}.}${CF[domain]}
 OLD_IP=$(dig $URL +short | awk '{ print; exit }')
 NEW_IP=$(dig myip.opendns.com @resolver1.opendns.com +short)
 
-# get the cloudflare record ID
-#ALL_RECORDS=curl https://www.cloudflare.com/api_json/ \
-curl https://www.cloudflare.com/api_json/ \
-	-d "email=${CF[email]}" \
-	-d "tkn=${CF[token]}" \
-	-d "z=${CF[domain]}" \
-	-d 'a=rec_load_all'
-#	-d 'a=stats' \
-
-# update the record with the new IP
-: <<'END'
-if [ ${OLD_IP} != ${NEW_IP} ] ;
+if [ ${CF[id]} != '' && $OLD_IP != $NEW_IP ] ;
 then
-	RESP=curl https://www.cloudflare.com/api_json/ \
+	curl https://www.cloudflare.com/api_json/ \
 		-d 'a=rec_edit' \
 		-d "email=${CF[email]}" \
 		-d "tkn=${CF[token]}" \
 		-d "z=${CF[domain]}" \
-		-d "id=${CF[id]}" \
+		-d "id=${CF[record]}" \
 		-d "content=${NEW_IP}" \
 		-d "display_content=${NEW_IP}"
 fi
-END
