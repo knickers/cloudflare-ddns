7a593e0db8904b2814ad38d4eeaa1cfcfc0812bd
Nick Cox <nickcox1008@gmail.com>
2020-11-01 23:23:58 -0700

    List only records that match config domain

--- list.sh
+++ list.sh
@@ -5,7 +5,11 @@ declare -A config=$( ./read-config.sh "$@" )
 
 API="https://api.cloudflare.com/client/v4/zones/${config[zone_id]}"
 
-curl "$API/dns_records?type=A" \
+if [ -n "${config[domain]}" ]; then
+	domain="&name=${config[domain]}"
+fi
+
+curl -v "$API/dns_records?type=A$domain" \
 	-H "Authorization: Bearer ${config[api_token]}" \
 	-H 'Content-Type:application/json' \
 
