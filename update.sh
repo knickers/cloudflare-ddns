d3f884dbea387b39216c54ca7491822c2e53a2a2
Nick Cox <nickcox1008@gmail.com>
2020-11-01 22:42:27 -0700

    Use config reader and update for API version 4

--- update.sh
+++ update.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 set -e
 
-declare -A CF
-while read line
-do
-	CF["${line%=*}"]="${line#*=}"
-done < cloudflare.conf
+declare -A config=$( ./read-config.sh "$@" )
 
-URL=${CF[subdomain]:+${CF[subdomain]}.}${CF[domain]}
-OLD_IP=$(dig $URL +short @ns.cloudflare.com | awk '{ print; exit }')
+for k in domain record_id; do
+	if [ -z "${config["$k"]}" ]; then
+		echo "Missing config item: $k"
+		exit 1
+	fi
+done
+
+OLD_IP=$(dig ${config[domain]} +short @ns.cloudflare.com | awk '{ print; exit }')
 NEW_IP=$(dig myip.opendns.com @resolver1.opendns.com +short)
 
-if [[ ${CF[record]} != '' && $OLD_IP != $NEW_IP ]]; then
-	curl https://www.cloudflare.com/api_json/ \
-		-d 'a=rec_edit' \
-		-d "email=${CF[email]}" \
-		-d "tkn=${CF[token]}" \
-		-d "z=${CF[domain]}" \
-		-d "id=${CF[record]}" \
-		-d 'ttl=1' \
-		-d 'type=A' \
-		-d "name=$URL" \
-		-d "content=$NEW_IP" \
-		-d "display_content=$NEW_IP"
+if [ $OLD_IP = $NEW_IP ]; then
+	echo 'Same IP address'
+	exit
 fi
+
+API="https://api.cloudflare.com/client/v4/zones/${config[zone_id]}"
+
+curl -X PATCH "$API/dns_records/${config[record_id]}" \
+	-H "Authorization: Bearer ${config[api_token]}" \
+	-H 'Content-Type:application/json' \
+	--data '{"content":"'"$NEW_IP"'"}'
+
+echo
